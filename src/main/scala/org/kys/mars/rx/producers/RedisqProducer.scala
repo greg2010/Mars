61b48ae51c931ba6e30cf00056206bf5b311e71d
Grigory Romodanovskiy <greg2008200@gmail.com>
2018-06-11 12:56:48 -0400

    prevent ssl handshake exception from crashing the service

--- src/main/scala/org/kys/mars/rx/producers/RedisqProducer.scala
+++ src/main/scala/org/kys/mars/rx/producers/RedisqProducer.scala
@@ -14,6 +14,8 @@ import io.circe.generic.auto._
 import monix.reactive.subjects.ReplaySubject
 import org.kys.mars.models.Json.{PackageBis, R00tJsonObject}
 
+import scala.util.control.NonFatal
+
 /** RedisQ producer class (Reactive Streams pattern).
   * This class can be used to produce a stream of [[PackageBis]]
   * that can be later transformed and processed by a consumer.
@@ -29,33 +31,38 @@ class RedisqProducer(override val rate: FiniteDuration)(key: String = "kys-km-pr
 
   override def unsafeSubscribeFn(subscriber: Subscriber[PackageBis]): Cancelable = {
     subscriber.scheduler.scheduleAtFixedRate(0.seconds, rate) {
-      val rawResp = r.send()
-      rawResp.body match {
-        case Right(succResp) =>
-          parse(succResp).map(_.as[R00tJsonObject]) match {
-            case Right(Right(res)) =>
-              res.`package` match {
-                case Some(pkg) => subscriber.onNext(pkg)
-                case _ => ()
-              }
-            case Left(err) =>
-              logger.error(s"Failed to parse response from RedisQ, " +
-                s"error=${err.message} " +
-                s"rawMessage=$succResp")
+      try {
+        val rawResp = r.send()
+        rawResp.body match {
+          case Right(succResp) =>
+            parse(succResp).map(_.as[R00tJsonObject]) match {
+              case Right(Right(res)) =>
+                res.`package` match {
+                  case Some(pkg) => subscriber.onNext(pkg)
+                  case _ => ()
+                }
+              case Left(err) =>
+                logger.error(s"Failed to parse response from RedisQ, " +
+                  s"error=${err.message} " +
+                  s"rawMessage=$succResp")
 
-            case Right(Left(err)) =>
-              logger.info(s"Failed to decode response from RedisQ, " +
-                s"error=${err.message} " +
-                s"rawMessage=$succResp")
-          }
-        case Left(err) =>
-          rawResp.code match {
-            case 429 =>
-              val delay = rawResp.header("Retry-After").map(_.toLong).getOrElse(10000L)
-              logger.warn(s"Got 429, delaying by delay=$delay")
-              Thread.sleep(delay)
-            case _ => logger.error(s"Got http error response from RedisQ, code=${rawResp.code} error=$err")
-          }
+              case Right(Left(err)) =>
+                logger.info(s"Failed to decode response from RedisQ, " +
+                  s"error=${err.message} " +
+                  s"rawMessage=$succResp")
+            }
+          case Left(err) =>
+            rawResp.code match {
+              case 429 =>
+                val delay = rawResp.header("Retry-After").map(_.toLong).getOrElse(10000L)
+                logger.warn(s"Got 429, delaying by delay=$delay")
+                Thread.sleep(delay)
+              case _ => logger.error(s"Got http error response from RedisQ, code=${rawResp.code} error=$err")
+            }
+        }
+      } catch {
+        case e: Exception if NonFatal(e) =>
+          logger.error("Caught unhandled exception in RedisqProducer", e)
       }
     }
   }
